9d2d6ebacffa55ba330725e3f08cb75a03302dba
Randy Glasgow <Glasgowrandy@gmail.com>
2025-02-23 19:03:26 -0700

    Remove Convex-related imports from inner.tsx

--- app/server/inner.tsx
+++ app/server/inner.tsx
@@ -1,11 +1,6 @@
 "use client";
 
-import { Preloaded } from "convex/react";
-import { api } from "../../convex/_generated/api";
-
-export default function Home(_: {
-  preloaded: Preloaded<typeof api.project.getProjects>;
-}) {
+export default function Home() {
   return (
     <>
       <div className="flex flex-col gap-4 bg-slate-200 dark:bg-slate-800 p-4 rounded-md">
