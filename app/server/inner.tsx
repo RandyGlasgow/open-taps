ca3bcf3e8d562453f5893ff1ed5b9db2f7b92e5e
Randy Glasgow <Glasgowrandy@gmail.com>
2025-02-23 19:01:39 -0700

    Remove preloaded query and mutation from inner.tsx

--- app/server/inner.tsx
+++ app/server/inner.tsx
@@ -1,22 +1,16 @@
 "use client";
 
-import { Preloaded, useMutation, usePreloadedQuery } from "convex/react";
+import { Preloaded } from "convex/react";
 import { api } from "../../convex/_generated/api";
 
-export default function Home({
-  preloaded,
-}: {
+export default function Home(_: {
   preloaded: Preloaded<typeof api.project.getProjects>;
 }) {
-  const data = usePreloadedQuery(preloaded);
-  const addNumber = useMutation(api.project.createProject);
   return (
     <>
       <div className="flex flex-col gap-4 bg-slate-200 dark:bg-slate-800 p-4 rounded-md">
         <h2 className="text-xl font-bold">Reactive client-loaded data</h2>
-        <code>
-          <pre>{JSON.stringify(data, null, 2)}</pre>
-        </code>
+        <code></code>
       </div>
     </>
   );
